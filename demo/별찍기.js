e496c9f54b3e57549bf9ae4b570f5770e186e24a
kangdari_ <ksh44820@naver.com>
2020-05-06 18:23:03 +0900

    별 찍기 수정

--- demo/별찍기.js
+++ demo/별찍기.js
@@ -11,4 +11,17 @@ process.stdin.on('data', data => {
         answer += '\n'
     }
     console.log(answer)
+
+});
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+    const n = data.split(" ");
+    const a = Number(n[0]), b = Number(n[1]);
+
+    
+    const row = `${'*'.repeat(a)}\n`
+    const col = row.repeat(b)
+    
+    console.log(col)
 });
