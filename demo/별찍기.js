afbfbbf009868da8bca254a6b7b9a286503a820c
kangdari_ <ksh44820@naver.com>
2020-05-06 21:50:33 +0900

    demo 수정

--- demo/별찍기.js
+++ demo/별찍기.js
@@ -19,7 +19,6 @@ process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
 
-    
     const row = `${'*'.repeat(a)}\n`
     const col = row.repeat(b)
     
