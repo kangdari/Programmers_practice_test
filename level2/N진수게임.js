a3e3da491aebabdcc2f0ce83149ada1b20497833
kangdari_ <ksh44820@naver.com>
2020-05-09 17:00:10 +0900

    :pencil: N진수 게임

---
+++ level2/N진수게임.js
@@ -0,0 +1,24 @@
+function solution(n, t, m, p){
+    let str = ''
+    let answer = ''
+    let count = 0;
+
+    // t*m , 말해야 할 개수 * 참여 인원 수
+    while(count < t*m ){
+        str += count.toString(n)
+        count++
+    }
+
+    let order = p - 1; // 첫 순서 (index는 0부터)
+    for(let i=0; i<t; i++){
+        answer += str.slice(order, order+1).toUpperCase()
+        order += m
+    }
+
+    console.log(answer)
+    return answer
+}
+
+solution(2,4,2,1)
+solution(16,16,2,1)
+solution(16,16,2,2)
